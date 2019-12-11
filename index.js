4f3bbc4e789cef86da8404be81b22320cd7c657d
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-11 18:09:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function iterativeLog(array) {
     console.log(`${index}: ${element}`)
   })
 }
+
+function iterate(callback) {
+  var array = ["Donut", "Pizza", "Salad"]
+
+  array.forEach (callback)
+
+  return array
+}
+
+function doToArray(array, callback) {
+  
+}
